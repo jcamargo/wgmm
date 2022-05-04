2f5917675421c552a30d806745bb5bab0966e77c
jcamargo <juliano.camargo@vito.be>
2022-05-04 17:42:24 +0200

    wrapped __main__

--- mnv_mix.py
+++ mnv_mix.py
@@ -34,81 +34,80 @@ def sample(w, mu_cov_lst, N):
     return np.concatenate(res, axis=1), (sel, Nk)
 
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
     # %%
-import matplotlib.pyplot as plt
-K = 3
-nd = 3
-print(random_mvn_mix(K))
-w, mu_cov_lst = random_mvn_mix(K, nd=nd)
+    import matplotlib.pyplot as plt
+    K = 3
+    nd = 3
+    print(random_mvn_mix(K))
+    w, mu_cov_lst = random_mvn_mix(K, nd=nd)
 
-# %%
-N = 10000
-X, ck = sample(w, mu_cov_lst, N)
-inds = np.cumsum(np.insert(ck[1], 0, 0.0))
-b = inds[:-1]
-e = inds[1:]
-tags = np.zeros(N)
-for k in range(K):
-    tags[b[k]:e[k]] = k
+    # %%
+    N = 10000
+    X, ck = sample(w, mu_cov_lst, N)
+    inds = np.cumsum(np.insert(ck[1], 0, 0.0))
+    b = inds[:-1]
+    e = inds[1:]
+    tags = np.zeros(N)
+    for k in range(K):
+        tags[b[k]:e[k]] = k
 
-# %%
-cols = 'rgbcmy'
-plt.clf()
-for k in range(K):
-    plt.scatter(*X[:2, b[k]:e[k]], s=3, alpha=0.025,
-                color=cols[k % len(cols)])
+    # %%
+    cols = 'rgbcmy'
+    plt.clf()
+    for k in range(K):
+        plt.scatter(*X[:2, b[k]:e[k]], s=3, alpha=0.025,
+                    color=cols[k % len(cols)])
 
-ax = plt.gca()
-ax.set_axis_off()
+    ax = plt.gca()
+    ax.set_axis_off()
 
-# %%
-import plotly.express as px
-from plotly.offline import plot
-fig = px.scatter_3d(x=X[0, :], y=X[1, :], z=X[2, :], color=tags)
-fig.update_traces(marker=dict(size=2), marker_coloraxis=None)
-plot(fig, filename='gmm_cloud.html')
+    # %%
+    import plotly.express as px
+    from plotly.offline import plot
+    fig = px.scatter_3d(x=X[0, :], y=X[1, :], z=X[2, :], color=tags)
+    fig.update_traces(marker=dict(size=2), marker_coloraxis=None)
+    plot(fig, filename='gmm_cloud.html')
 
+    # %% EM iterations E-step
+    # https://en.wikipedia.org/wiki/Expectation%E2%80%93maximization_algorithm#E_step
+    w_ref, mu_cov_ref = w, mu_cov_lst
 
-# %% EM iterations E-step
-# https://en.wikipedia.org/wiki/Expectation%E2%80%93maximization_algorithm#E_step
-w_ref, mu_cov_ref = w, mu_cov_lst
+    # starting guess
+    w_est, mu_cov_lst_est = random_mvn_mix(K, nd=nd)
+    w_score = np.zeros((K, N))
 
-# starting guess
-w_est, mu_cov_lst_est = random_mvn_mix(K, nd=nd)
-w_score = np.zeros((K, N))
+    # %%
+    # Calculate membership probabilities w_score
+    for k, mu_cov in enumerate(mu_cov_lst_est):
+        w_score[k, :] = w_est[k]*st.multivariate_normal(*mu_cov).pdf(X.T)
+    # normalizing
+    w_score /= np.sum(w_score, axis=0)
+    # result of the soft assignment using starting guess
+    z_sel = np.argmax(w_score, axis=0)
 
-# %%
-# Calculate membership probabilities w_score
-for k, mu_cov in enumerate(mu_cov_lst_est):
-    w_score[k, :] = w_est[k]*st.multivariate_normal(*mu_cov).pdf(X.T)
-# normalizing
-w_score /= np.sum(w_score, axis=0)
-# result of the soft assignment using starting guess
-z_sel = np.argmax(w_score, axis=0)
+    # % Now recalculate parameters
+    w_est = np.sum(w_score, axis=1)/N
+    for k in range(K):
+        mu = (w_score[k]*X).sum(axis=1) / (w_est[k]*N)
+        # not sure if cov formula is ok
+        cov = (w_score[k]*(X - np.vstack(mu))) @ (X - np.vstack(mu)).T
+        cov /= (w_est[k]*N)
+        mu_cov_lst_est[k] = (mu, cov)
 
-# % Now recalculate parameters
-w_est = np.sum(w_score, axis=1)/N
-for k in range(K):
-    mu = (w_score[k]*X).sum(axis=1) / (w_est[k]*N)
-    # not sure if cov formula is ok
-    cov = (w_score[k]*(X - np.vstack(mu))) @ (X - np.vstack(mu)).T
-    cov /= (w_est[k]*N)
-    mu_cov_lst_est[k] = (mu, cov)
+    print("w=", w_est)
+    print("mu_cov_lst=", mu_cov_lst_est)
 
-print("w=", w_est)
-print("mu_cov_lst=", mu_cov_lst_est)
+    # %%
+    mu_arr = np.vstack([mu_cov[0] for mu_cov in mu_cov_lst_est]).T
+    fig = px.scatter_3d(x=X[0, :], y=X[1, :], z=X[2, :], color=z_sel)
+    fig.update_traces(marker=dict(size=1), marker_coloraxis=None)
+    fig.add_scatter3d(mode="markers", x=mu_arr[0], y=mu_arr[1], z=mu_arr[2])
+    plot(fig, filename='gmm_cloud.html')
 
-# %%
-mu_arr = np.vstack([mu_cov[0] for mu_cov in mu_cov_lst_est]).T
-fig = px.scatter_3d(x=X[0, :], y=X[1, :], z=X[2, :], color=z_sel)
-fig.update_traces(marker=dict(size=1), marker_coloraxis=None)
-fig.add_scatter3d(mode="markers", x=mu_arr[0], y=mu_arr[1], z=mu_arr[2])
-plot(fig, filename='gmm_cloud.html')
-
-# %%
-#
-# TODO Plot log likelihood on the sample data? That will increase with the
-# steps
-#
-# TODO
+    # %%
+    #
+    # TODO Plot log likelihood on the sample data? That will increase with the
+    # steps
+    #
+    # TODO
